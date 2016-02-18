300366fc8ef6ca086dcaca2c710de09f4fdc1be1
lixi <xianglei.li@eurecom.fr>
2016-02-18 23:56:57 +0100

    Update Step 1: remove tweet corpus and refine the interface

--- src/fr/eurecom/sentime/SentimeSystem.java
+++ src/fr/eurecom/sentime/SentimeSystem.java
@@ -2,6 +2,10 @@ package fr.eurecom.sentime;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -16,129 +20,171 @@ public class SentimeSystem {
 	static String PATH = "";
 	private static long startTime = System.currentTimeMillis();
 	
+	private static void help(){
+		System.err.println("Usage: <System Mode> <tweet corpus> [parameters] ... \n");
+		System.err.println("SYSTEM MODE: ");
+		System.err.println("train:  This mode is used for training the models.");
+		System.err.println("test:   This mode is used for testing the models.");
+		System.err.println("single: This mode is used for classifying a single tweet.\n");
+		System.err.println("PARAMETERS: ");
+		System.err.println("-arrfname         Specify the name of arffs which are used to store the model");
+		System.err.println("-testmodel        Only test one sub-classifier, use 0-4 to specify which sub-classifier to be tested");
+		System.err.println("-trainmodel       Only train one sub-classifier, use 0-3 to specify which sub-classifer to be trained");
+		System.err.println("-bsize            Enable bagging training process and specify the size of bootstrap samples.");
+		System.err.println("-experiment       Using <nostanford> to disable Stanford Sentiment System; using <noteamx> to exclude TeamX; using <nost> to exclude both systems");
+		System.err.println("-disablefilter    Disable the default filter mechanism: using <train> to disable duplicate input tweets; using <test> to disable duplicate tweet filtering when scoring.");
+	}
+	
 	public static void main(String[] args) throws Exception{
 		
-		String nameOfNRCTrain = "";
-		String nameOfGUMLTTrain = "";
-		String nameOfKLUETrain = "";
-		String nameOfTeamXTrain = "";
+		// Names used for identifying arff files. If it's not specified, default arff files will be used.
+		String nameOfNRCarff = "";
+		String nameOfGUMLTarff = "";
+		String nameOfKLUEarff = "";
+		String nameOfTeamXarff = "";
 		String nameOfTheOne = "";
-		int bootstrapNumber = 0;
-		Tweet singleTweet;
-		//String nameOfTeamXTrain = "";
-		int evalmodelmode = 0;
-		int trainmodelmode = 0;
-		boolean stanford = false;
-		boolean without = false;
-		boolean nofilter = false;
-		boolean score = false;
-		Options options = new Options();
+		String nameOfOutput = "";
 		
-		options.addOption("on", true, "output Name");
-		options.addOption("tf", true, "Name of the NRC Trainfile");
-		options.addOption("tf2", true, "Name of the GU-MLT-LT Trainfile");
-		options.addOption("tf3", true, "Name of the KLUE Trainfile");
-	    options.addOption("tf4", true, "Name of the TeamX Trainfile");
-	    options.addOption("trainfile", true, "Name of all the train file");
-		options.addOption("em", true, "Eval Modelmode");
-		options.addOption("tm", true, "Train Modelmode");
-		options.addOption("stanford", true, "Enable Stanford System In Ensemble");
-		options.addOption("without", true, "Exclude TeamX");
-		options.addOption("bn", true, "Bootstrap number");
-		options.addOption("nofilter", true, "Do not filter out the duplicate tweets when reading from the file");
-		options.addOption("score", true, "Do not filter out the duplicate tweets when scoring");
+		// Specifying the size of bootstrap samples.
+		int bootstrapNumber = 0;
+		
+		// Storing the single tweet.
+		Tweet singleTweet;
+		
+		// System mode indicators.
+		boolean bagging = false;        // When true, enable bagging training process
+		boolean sub_classifier = false; // When true, train or test on one sub-classifier
+		int evalmodelmode = 0;          // Indicating which model to be tested; ex: 0 represents NRC
+		int trainmodelmode = 0;         // Indicating which model to be trained
+		boolean stanford = true;        // When true, enable stanford sentiment system
+		boolean without = false;        // When true, disable TeamX
+		boolean nofilter = false;       // When true, do not filter out duplicate tweet
+		boolean score = false;          // When true, use the same scoring mechanism as SemEval2015
+		
+		// Parse the arguments
+		if(args.length<1){
+			System.out.println("Usage: <System Mode> <tweet corpus> [parameters] ...\nUsing -help to see detail information.");
+			return;
+		}
+
+		Options options = new Options();
+	    options.addOption("arffname", true, "Name of all the arff files");
+		options.addOption("testmodel", true, "Choose the model to be tested");
+		options.addOption("trainmodel", true, "Choose the model to be trained");
+		options.addOption("experiment", true, "Enable different experiment modes");
+		options.addOption("bsize", true, "Using Bagging algorithm and set the size of bootstrap samples");
+		options.addOption("disablefilter", true, "Disable filtering mechanism");
+		options.addOption("help", false, "Offering tutorials");
 		
 		CommandLineParser parser = new GnuParser();
 		try {
-			String name = "";
 			CommandLine line = parser.parse(options, args);
-			if(line.hasOption("on")){
-				name = "_" + line.getOptionValue("on");
+			if(line.hasOption("arffname")){
+				nameOfNRCarff = "Trained-Features-NRC_" + line.getOptionValue("arffname");
+				nameOfGUMLTarff = "Trained-Features-GUMLTLT_" + line.getOptionValue("arffname");
+				nameOfKLUEarff = "Trained-Features-KLUE_" + line.getOptionValue("arffname");
+				nameOfTeamXarff = "Trained-Features-TeamX_" + line.getOptionValue("arffname");
+				nameOfOutput = line.getOptionValue("arffname");
 			}
-			if(line.hasOption("tf")){
-				nameOfNRCTrain = line.getOptionValue("tf");
+			if(line.hasOption("testmodel")){
+				evalmodelmode = Integer.parseInt(line.getOptionValue("testmodel"));
+				sub_classifier = true;
 			}
-			if(line.hasOption("tf2")){
-				nameOfGUMLTTrain = line.getOptionValue("tf2");
-			}			
-			if(line.hasOption("tf3")){
-				nameOfKLUETrain = line.getOptionValue("tf3");
+			if(line.hasOption("trainmodel")){
+				trainmodelmode = Integer.parseInt(line.getOptionValue("trainmodel"));
+				sub_classifier = true;
 			}
-	        if(line.hasOption("tf4")){
-	            nameOfTeamXTrain = line.getOptionValue("tf4");
-	        }
-			if(line.hasOption("em")){
-				evalmodelmode = Integer.parseInt(line.getOptionValue("em"));
+			if(line.hasOption("experiment")){
+				switch(line.getOptionValue("experiment")){
+				case "nostanford":
+					stanford = false;
+					break;
+				case "noteamx":
+					without = true;
+					break;
+				case "nost":
+					stanford = false;
+					without = true;
+					break;
+				default:
+					System.err.println("Wrong parameters for -experiment!!\nUsing -help to see detail information.");
+					return;
+				}
 			}
-			if(line.hasOption("tm")){
-				trainmodelmode = Integer.parseInt(line.getOptionValue("tm"));
+			if(line.hasOption("bsize")){
+				bootstrapNumber = Integer.parseInt(line.getOptionValue("bsize"));
+				bagging = true;
 			}
-			if(line.hasOption("stanford")){
-				stanford = true;
+			if(line.hasOption("disablefilter")){
+				switch(line.getOptionValue("disablefilter")){
+				case "train":
+					nofilter = true;
+					break;
+				case "test":
+					score = true;
+					break;
+				default:
+					System.err.println("Wrong parameters for -disablefilter!!\nUsing -help to see detail information.");
+					return;
+				}
 			}
-			if(line.hasOption("without")){
-				without = true;
-			}
-			if(line.hasOption("bn")){
-				bootstrapNumber = Integer.parseInt(line.getOptionValue("bn"));
-			}
-			if(line.hasOption("trainfile")){
-				nameOfNRCTrain = "Trained-Features-NRC_" + line.getOptionValue("trainfile");
-				nameOfGUMLTTrain = "Trained-Features-GUMLTLT_" + line.getOptionValue("trainfile");
-				nameOfKLUETrain = "Trained-Features-KLUE_" + line.getOptionValue("trainfile");
-				nameOfTeamXTrain = "Trained-Features-TeamX_" + line.getOptionValue("trainfile");
-			}
-			if(line.hasOption("nofilter")){
-				nofilter = true;
-			}
-			if(line.hasOption("nofilter")){
-				score = true;
+			if(line.hasOption("help")){
+				help();
+				return;
 			}
 	
 			
 			String[] argList = line.getArgs();
-			if(argList[0].equals("single")){
+			PATH = argList[1];
+			SentimeRequestHandler sentimentanalysis = new SentimeRequestHandler(PATH, nofilter, score);
+			
+			switch (argList[0]){
+			case "train":
+				if(sub_classifier){
+					switch(trainmodelmode){
+					case 0: nameOfTheOne = nameOfNRCarff;
+					break;
+					case 1: nameOfTheOne = nameOfGUMLTarff;
+					break;
+					case 2: nameOfTheOne = nameOfKLUEarff;
+					break;
+					case 3: nameOfTheOne = nameOfTeamXarff;
+					break;
+					}
+					sentimentanalysis.trainSystem(trainmodelmode, nameOfTheOne);
+				} else if (bagging){
+					sentimentanalysis.bootstrapAllSystems(trainmodelmode, nameOfOutput, bootstrapNumber, without);
+				} else {
+					sentimentanalysis.trainAllSystems(trainmodelmode, nameOfOutput);
+				}
+				break;
+			case "test":
+				if(sub_classifier){
+					switch(evalmodelmode){
+					case 0: nameOfTheOne = nameOfNRCarff;
+					break;
+					case 1: nameOfTheOne = nameOfGUMLTarff;
+					break;
+					case 2: nameOfTheOne = nameOfKLUEarff;
+					break;
+					case 3: nameOfTheOne = nameOfTeamXarff;
+					break;
+					}
+					sentimentanalysis.testSystem(evalmodelmode, nameOfTheOne);
+				} else {
+					sentimentanalysis.testAllSystem(nameOfNRCarff, nameOfGUMLTarff, nameOfKLUEarff, nameOfTeamXarff, stanford, without);
+				}
+				break;
+			case "single":
 				BufferedReader strin=new BufferedReader(new InputStreamReader(System.in));
 				System.out.println("Please enter the tweet:"); 
 				String str = strin.readLine();
 				singleTweet= new Tweet(str, "neutral", "1");
-				SentimeRequestHandler sentimentanalysis = new SentimeRequestHandler(singleTweet);
-				sentimentanalysis.process(nameOfNRCTrain, nameOfGUMLTTrain, nameOfKLUETrain);
-				return;
-			}
-			
-			PATH = argList[1];
-			
-			SentimeRequestHandler sentimentanalysis = new SentimeRequestHandler(PATH, nofilter, score);
-			
-			switch (argList[0]){
-				case "eval":
-					switch(evalmodelmode){
-					case 0: nameOfTheOne = nameOfNRCTrain;
-					break;
-					case 1: nameOfTheOne = nameOfGUMLTTrain;
-					break;
-					case 2: nameOfTheOne = nameOfKLUETrain;
-					break;
-					case 3: nameOfTheOne = nameOfTeamXTrain;
-					break;
-					}
-					sentimentanalysis.testSystem(evalmodelmode, nameOfTheOne);
-					break;
-				case "evalAll":
-					sentimentanalysis.testAllSystem(nameOfNRCTrain, nameOfGUMLTTrain, nameOfKLUETrain, nameOfTeamXTrain, stanford, without);
-					break;
-				case "train":
-					sentimentanalysis.trainSystem(trainmodelmode, name);
-					break;
-				case "trainAll":
-					sentimentanalysis.trainAllSystems(trainmodelmode, name);
-					break;
-				case "bootstrap":
-					sentimentanalysis.bootstrapAllSystems(trainmodelmode, name, bootstrapNumber, without);
-					break;
-				default:
-					throw new IllegalArgumentException("Invalid mode: " + argList[0]);
+				SentimeRequestHandler sentimentpipline = new SentimeRequestHandler(singleTweet);
+				sentimentpipline.process(nameOfNRCarff, nameOfGUMLTarff, nameOfKLUEarff);
+				break;
+			default:
+				throw new IllegalArgumentException("Invalid mode: " + argList[0]);
 			}					
 		}
 		catch(ParseException exp){
